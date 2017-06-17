ba743eea5d51aa0edfcc1a2ee9c669dac337d103
alexfevery <alexfevery@gmail.com>
2017-06-17 17:47:46 -0400

    adjust gitignore

--- Content.cs
+++ Content.cs
@@ -61,6 +61,7 @@ namespace ProjectCobalt
                 int id = GL.GenTexture();
                 GL.BindTexture(TextureTarget.Texture2D, id);
                 Bitmap bmp = new Bitmap("GameContent/TestTexture.png");
+
                 bmp.RotateFlip(RotateFlipType.Rotate180FlipNone);
                 BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
                 GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
